bb2efaf44cf7b86eb409f10857ccf7b52f4459b5
Konogan Berger <kono@eduroam-n2ng-146157.local.univ-savoie.fr>
2020-10-09 15:37:26 +0200

    Retravail Header

--- header.php
+++ header.php
@@ -1,22 +1,8 @@
 <html>
   <head>
     <title> OPAL </title>
-    <link href="style.css">
+    <link href="style.css" type="text/css">
   </head>
-<<<<<<< HEAD
 
 
   <body>
-<<<<<<< HEAD
-<h1>salut c'est camille</h1>
-
-  </body>
-
-
-
-
-</html>
-=======
->>>>>>> 5b4e116d2ccb22ff2f4d4bb583681c8d14ffdddb
-=======
->>>>>>> 7309c6f1235078d51ba35d213b5190fad6ed3c22
